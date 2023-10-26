fad424fa3b74e9466529ecc7d3452ab8e71116e5
StruzhakS <111219358+StruzhakS@users.noreply.github.com>
2023-10-26 15:39:05 +0000

    Deploying to gh-pages from @ StruzhakS/test-task-3@1edd8dbf61e3b0b8db3be307a5caed81daa42778 🚀

---
+++ static/js/475.cdb874cb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktest_task_3=self.webpackChunktest_task_3||[]).push([[475],{475:function(t,e,l){l.r(e),l.d(e,{default:function(){return n}});l(791);var s="DummyTable_tableBox__tY6-2",i=l(184),n=function(t){var e=t.title;return(0,i.jsx)("div",{className:s,children:(0,i.jsx)("table",{children:(0,i.jsx)("thead",{children:(0,i.jsxs)("tr",{children:[(0,i.jsx)("td",{children:null===e||void 0===e?void 0:e.split(" ")[0]}),(0,i.jsx)("td",{children:null===e||void 0===e?void 0:e.split(" ")[1]})]})})})})}}}]);
+//# sourceMappingURL=475.cdb874cb.chunk.js.map
